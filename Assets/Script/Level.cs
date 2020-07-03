1460ab737328d33598eee50fd3a2f75318ce8b4b
AlexGearX <alexgear34@gmail.com>
2020-07-03 15:45:09 +0200

    first commit + Splash,menu and game background

---
+++ Assets/Script/Level.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Level : MonoBehaviour
+{
+    [SerializeField] int timeToWait = 4;
+    int currentSceneIndex;
+    void Start()
+    {
+
+        currentSceneIndex = SceneManager.GetActiveScene().buildIndex ;
+        if (currentSceneIndex == 0)
+        {
+            StartCoroutine(LoadMenuScreenAfterSplash());
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    IEnumerator LoadMenuScreenAfterSplash()
+    {
+        yield return new WaitForSeconds(timeToWait);
+        LoadNextScene();
+    }
+
+    public void LoadNextScene()
+    {
+        SceneManager.LoadScene(currentSceneIndex + 1);
+    }
+
+    public void LoadGame()
+    {
+        SceneManager.LoadScene("Game");
+    }
+
+    public void LoadGameOver()
+    {   
+       
+    }
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+}
